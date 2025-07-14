268c9ba40686c3d00e8cdae3f8cc36aaf888ac13
Alex-Alessi <alex.alessi246@gmail.com>
2025-07-14 12:25:09 +0200

    Modale aggiornata

--- src/components/Mymodal.jsx
+++ src/components/Mymodal.jsx
@@ -14,6 +14,10 @@ export default function Mymodal({
   abilities,
   stats,
 }) {
+  function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
   return (
     <Modal
       size="lg"
@@ -34,14 +38,14 @@ export default function Mymodal({
         <img
           src={image}
           alt={name}
-          style={{ display: "flex", width: "150px", margin: "0 auto" }}
+          style={{ display: "flex", width: "20rem", margin: "0 auto" }}
         />
         {types && typeColors ? (
           types.map((type, i) => (
             <span
+              key={i}
               style={{
                 backgroundColor: typeColors[i],
-
                 textAlign: "center",
                 fontSize: "1rem",
                 fontWeight: "bold",
@@ -52,13 +56,38 @@ export default function Mymodal({
                 height: "25px",
               }}
             >
-              <p key={i}>{type}</p>
+              <p>{type}</p>
             </span>
           ))
         ) : (
           <p>Caricamento Tipi...</p>
         )}
-        <p>Qui puoi mettere contenuti dettagliati del Pokémon cliccato.</p>
+        <ul>
+          <li>Altezza: {height}</li>
+          <li>Peso: {weight}</li>
+          <li>Exp di base: {base_experience}</li>
+          <li>
+            Abilità:{" "}
+            <ul>
+              {abilities?.map((a, i) => (
+                <li key={i}>
+                  {capitalizeFirstLetter(a.name)}{" "}
+                  {a.ishidden ? "(nascosta)" : ""}
+                </li>
+              ))}
+            </ul>
+          </li>
+          <li>
+            Statistiche:{" "}
+            <ul>
+              {stats?.map((s, i) => (
+                <li key={i}>
+                  {capitalizeFirstLetter(s.name)}: {s.base_stat}
+                </li>
+              ))}
+            </ul>
+          </li>
+        </ul>
       </Modal.Body>
     </Modal>
   );
